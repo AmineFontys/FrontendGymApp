494d20516566d6efbfe0bd063d708b228d4c97f9
Amine El-Mohamadi <466497@student.fontys.nl>
2024-06-07 11:49:06 +0200

    composefile changes

--- src/app/trainer/trainer.component.ts
+++ src/app/trainer/trainer.component.ts
@@ -29,10 +29,23 @@ export class TrainerComponent {
   constructor( private trainerService: TrainerService, private router : Router) {}
 
   ngOnInit(): void {
-    this.trainerService.getTrainers().subscribe((trainers) => {
-      this.trainers = trainers.filter(trainer => trainer.role === 1); // Filter for role number 1
-      console.log(this.trainers);
+    this.getTrainers();
+  }
+  getTrainers(): void {
+    this.trainerService.getTrainers().subscribe({
+      next: (response) => {
+        console.log('Trainers fetched successfully', response);
+        this.trainers = response;
+      },
+      error: (error) => {
+        console.error('Error:', error);
+      }
     });
+
+    // this.trainerService.getTrainers().subscribe((trainers) => {
+    //   this.trainers = trainers.filter(trainer => trainer.role === 1); // Filter for role number 1
+    //   console.log(this.trainers);
+    // });
   }
   selectTrainer(trainerId: number): void {
     this.selectedTrainerId = trainerId;
